768464b01a408e3b6700e4e452fc780fd93a1944
arcstep <43801@qq.com>
2023-08-26 17:44:43 +0800

    重构工作日算法

--- R/workdays.R
+++ R/workdays.R
@@ -1,89 +1,80 @@
 ##
-add_workdays0 <- function(d, from, to, newName = "工作日数",
-                          path = NULL, ignoreFrom = FALSE, ignoreTo = FALSE) {
-  stopifnot("data.frame" %in% class(d))
-  ## 构造需要处理的数据集
+distinct0 <- function(d, columns) {
+  d |> distinct(!!!dplyr::syms(columns[!is.null(columns)]))
+}
+
+#' @title 计算时间段内包含的工作日序列
+#' @description
+#' 如果需要按照ID排除工作日数量，则要求调用该函数。
+#' 
+#' @param d 要处理的数据集
+#' @param fromColumn 开始日期字段名
+#' @param toColumn 截止日期字段名
+#' @param idColumn 合并工作日依据的ID字段名称
+#' @param fromIgnore 计算工作日时忽略开始列
+#' @param toIgnore 计算工作日时忽略结束列
+#' @param path 指定节假日设定文件
+#' @export
+workdays_seq <- function(d, fromColumn, toColumn, idColumn = NULL, fromIgnore = TRUE, toIgnore = TRUE, path = NULL) {
   d0 <- d |>
-    mutate(`@from` = lubridate::as_date(d[[from]])) |>
-    mutate(`@to` = lubridate::as_date(d[[to]]))
-  ## 复杂性降低后的数据集
-  ds <- d0 |>
-    distinct(`@from`, `@to`) |>
-    mutate(`@from` = lubridate::as_date(`@from`)) |>
-    mutate(`@to` = lubridate::as_date(`@to`))
+    mutate(`@from` = lubridate::as_date(!!dplyr::sym(fromColumn))) |>
+    mutate(`@to` = lubridate::as_date(!!dplyr::sym(toColumn)))
   ##
-  fromDay <- ds$`@from` |> purrr::discard(~ is.na(.x)) |> min()
-  toDay <- ds$`@to` |> purrr::discard(~ is.na(.x)) |> max()
-  wd <- workdays_zh_CN(fromDay, toDay, path) |>
-    mutate(工作日标记 = ifelse(工作日标记, 累计工作日数, 0L))
-  ## 生成日期序列数据集
-  ds0 <- ds |>
-    left_join(wd, by = c("@from"="日期")) |>
-    rename(`@seq_from` = 累计工作日数, `@flag_from` = 工作日标记) |>
-    left_join(wd, by = c("@to"="日期")) |>
-    rename(`@seq_to` = 累计工作日数, `@flag_to` = 工作日标记) |>
-    mutate(
-      `@workdays` = purrr::pmap(
-        list(`@seq_from`, `@seq_to`, `@flag_from`, `@flag_to`),
-        function(seq_from, seq_to, flag_from, flag_to) {
-          ignoreSeqs <- 0L
-          if(ignoreFrom) { ignoreSeqs <- c(ignoreSeqs, flag_from) }
-          if(ignoreTo) { ignoreSeqs <- c(ignoreSeqs, flag_to) }
-          q <- seq(seq_from, seq_to)
-          q[!(q %in% ignoreSeqs)] + as.integer(fromDay)
-        })) |>
-    mutate(!!dplyr::sym(newName) := purrr::map_int(`@workdays`, ~ .x |> length()))
+  fromDay <- d0$`@from` |> purrr::discard(~ is.na(.x)) |> min()
+  toDay <- d0$`@to` |> purrr::discard(~ is.na(.x)) |> max()
+  wd <- workdays_zh_CN(fromDay, toDay, path) |> filter(工作日标记)
+  resp <- d0 |>
+    filter(!is.na(`@from`) & !is.na(`@to`)) |>
+    distinct0(c("@from", "@to", idColumn)) |>
+    mutate(`@seq` = purrr::map2(`@from`, `@to`, ~ .x:.y |> lubridate::as_date())) |>
+    tidyr::unnest(cols = c(`@seq`)) |>
+    semi_join(wd, by = c("@seq"="日期"))
   ##
-  x_by <- "@from"
-  y_by <- "@to"
-  names(x_by) <- from
-  names(y_by) <- to
-  d0 |>
-    left_join(ds0 |> select(`@from`, `@to`, `@workdays`, !!dplyr::sym(newName)), by = c("@from", "@to")) |>
-    select(-`@from`, -`@to`)
+  if(fromIgnore) {
+    dFrom <- resp$`@seq` != resp$`@from`
+  } else {
+    dFrom <- TRUE
+  }
+  if(toIgnore) {
+    dTo <- resp$`@seq` != resp$`@to`
+  } else {
+    dTo <- TRUE
+  }
+  resp |> filter(dFrom & dTo)
 }
 
 #' @title 计算时间段内包含的工作日数量
-#' @param d 要补充字段的数据集
-#' @param from 开始日期字段名
-#' @param to 截止日期字段名
+#' @description
+#' 支持按照ID字段合并
+#' 
+#' @param d 要处理的数据集
+#' @param fromColumn 开始日期字段名
+#' @param toColumn 截止日期字段名
+#' @param idColumn 合并工作日依据的ID字段名称
 #' @param newName 工作日字段命名
+#' @param fromIgnore 计算工作日时忽略开始列
+#' @param toIgnore 计算工作日时忽略结束列
 #' @param path 指定节假日设定文件
-#' @param ignoreFrom 计算工作日时忽略开始列
-#' @param ignoreTo 计算工作日时忽略结束列
-#' @param withSeq 返回工作日的标记序列，以便后续计算
 #' @export
-add_workdays <- function(d, from, to, newName = "工作日数",
-                         path = NULL, ignoreFrom = FALSE, ignoreTo = FALSE, withSeq = FALSE) {
-  resp <- add_workdays0(d, from, to, newName, path, ignoreFrom, ignoreTo)
-  if(withSeq) {
-    resp
+workdays_count <- function(d, fromColumn, toColumn, idColumn = NULL, newName = "工作日数", fromIgnore = TRUE, toIgnore = TRUE, path = NULL) {
+  d0 <- d |>
+    mutate(`@from` = lubridate::as_date(!!dplyr::sym(fromColumn))) |>
+    mutate(`@to` = lubridate::as_date(!!dplyr::sym(toColumn)))
+  if(is.null(idColumn)) {
+    resp <- d0 |>
+      left_join(
+        workdays_seq(d0, fromColumn, toColumn, idColumn, fromIgnore, toIgnore, path) |>
+          count(`@from`, `@to`, name = "@n"),
+        by = c("@from", "@to")) |>
+      select(-`@from`, -`@to`)
   } else {
-    resp |> select(-`@workdays`)
+    resp <- workdays_seq(d0, fromColumn, toColumn, idColumn, fromIgnore, toIgnore) |>
+      distinct0(c(idColumn, "@seq")) |>
+      count(!!dplyr::sym(idColumn), name = "@n")
   }
+  resp |>
+    tidyr::replace_na(list("@n" = 0L)) |>
+    rename(!!dplyr::sym(newName) := `@n`)
 }
 
-#' @title 计算时间段内包含的工作日数量，按照ID合并
-#' @param d 要补充字段的数据集
-#' @param from 开始日期字段名
-#' @param to 截止日期字段名
-#' @param idName 合并工作日依据的ID字段名称
-#' @param newName 工作日字段命名
-#' @param path 指定节假日设定文件
-#' @param ignoreFrom 计算工作日时忽略开始列
-#' @param ignoreTo 计算工作日时忽略结束列
-#' @param withSeq 返回工作日的标记序列，以便后续计算
-#' @export
-add_workdays_by_id <- function(d, from, to, idName, newName = "工作日数",
-                               path = NULL, ignoreFrom = FALSE, ignoreTo = FALSE, withSeq = FALSE) {
-  resp <- add_workdays(d, from, to, newName, path, ignoreFrom, ignoreTo, withSeq = T) |>
-    group_by(!!!dplyr::syms(idName)) |>
-    reframe(`@workdays` = list(unlist(`@workdays`) |> unique()), `@count` = length(`@workdays`[[1]])) |>
-    rename(!!dplyr::sym(newName) := `@count`)
-  if(withSeq) {
-    resp
-  } else {
-    resp |> select(-`@workdays`)
-  }
-}
 
--- tests/testthat/test-workdays.R
+++ tests/testthat/test-workdays.R
@@ -1,6 +1,6 @@
 library(tibble)
 
-test_that("计算工作日时，普通情况", {
+test_that("普通情况", {
   d <- tribble(
     ~x, ~y, ~z,
     "2023-1-1", "2023-1-10", 1:100000,
@@ -8,11 +8,11 @@ test_that("计算工作日时，普通情况", {
     "2023-1-1", "2023-1-10", 1:10000,
     "2023-1-11", "2023-1-21", 1:10000) |>
     tidyr::unnest(z)
-  resp <- add_workdays(d, "x", "y")
+  resp <- workdays_count(d, "x", "y", fromIgnore = T, toIgnore = F)
   resp$工作日数[[1]] |> testthat::expect_equal(6)
 })
 
-test_that("计算工作日时，根据ID合并工作日数", {
+test_that("根据ID合并工作日数", {
   d <- tribble(
     ~x, ~y, ~z,
     "2023-1-1", "2023-1-10", 1,
@@ -21,42 +21,45 @@ test_that("计算工作日时，根据ID合并工作日数", {
     "2023-1-8", "2023-1-21", 3,
     "2023-1-8", "2023-1-15", 4,
     "2023-1-11", "2023-1-21", 4)
-  resp <- add_workdays_by_id(d, "x", "y", id = "z")
+  resp <- workdays_count(d, "x", "y", idColumn = "z", fromIgnore = F, toIgnore = F)
   resp$工作日数[[1]] |> testthat::expect_equal(resp$工作日数[[2]])
   resp$工作日数[[3]] |> testthat::expect_equal(resp$工作日数[[4]])
 })
 
-test_that("计算工作日时，不计算开始日期", {
+test_that("不计算开始日期或结束日期", {
   d <- tribble(
     ~x, ~y, ~z,
     "2023-1-1", "2023-1-10", 1,
     "2023-1-5", "2023-1-10", 2,
     "2023-1-1", "2023-1-10", 3,
     "2023-1-11", "2023-1-21", 4)
-  add_workdays(d, "x", "y")$工作日数 |>
-    testthat::expect_equal(c(6, 4, 6, 8))
-  add_workdays(d, "x", "y", ignoreFrom = T)$工作日数 |>
-    testthat::expect_equal(c(6, 3, 6, 7))
-  add_workdays(d, "x", "y", ignoreTo = T)$工作日数 |>
-    testthat::expect_equal(c(5, 3, 5, 8))
-  add_workdays(d, "x", "y", ignoreFrom = T, ignoreTo = T)$工作日数 |>
-    testthat::expect_equal(c(5, 2, 5, 7))
+  workdays_count(d, "x", "y", fromIgnore = T, toIgnore = F)$工作日数 |> testthat::expect_equal(c(6, 3, 6, 7))
+  workdays_count(d, "x", "y", fromIgnore = F, toIgnore = T)$工作日数 |> testthat::expect_equal(c(5, 3, 5, 8))
+  workdays_count(d, "x", "y", fromIgnore = T, toIgnore = T)$工作日数 |> testthat::expect_equal(c(5, 2, 5, 7))
+  
+  d <- tribble(
+    ~x, ~y, ~z,
+    "2023-1-1", "2023-1-10", 1,
+    "2023-1-1", "2023-1-5", 2,
+    "2023-1-6", "2023-1-10", 2,
+    "2023-1-8", "2023-1-21", 3,
+    "2023-1-9", "2023-1-15", 4,
+    "2023-1-11", "2023-1-21", 4)
+  resp <- workdays_count(d, "x", "y", idColumn = "z", fromIgnore = T, toIgnore = F)
+  resp$工作日数 |> testthat::expect_equal(c(6, 5, 10, 9))
 })
 
-test_that("计算工作日时，开始日期和结束日期相同", {
+test_that("开始日期和结束日期相同", {
+  d <- tribble(
+    ~x, ~y, ~z,
+    "2023-1-1", "2023-1-1", 1,
+    "2023-1-5", "2023-1-5", 2,
+    "2023-1-15", "2023-1-15", 3,
+    "2023-1-21", "2023-1-21", 4)
+  workdays_count(d, "x", "y")$工作日数 |> testthat::expect_equal(c(0, 0, 0, 0))
+  workdays_count(d, "x", "y", fromIgnore = T, toIgnore = F)$工作日数 |> testthat::expect_equal(c(0, 0, 0, 0))
+  workdays_count(d, "x", "y", fromIgnore = F, toIgnore = T)$工作日数 |> testthat::expect_equal(c(0, 0, 0, 0))
 })
 
-test_that("计算工作日时，开始日期为假日，结束为工作日", {
-})
-
-test_that("计算工作日时，开始日期为假日，结束为假日", {
-})
-
-test_that("计算工作日时，开始日期为工作日，结束为工作日", {
-})
-
-test_that("计算工作日时，开始日期为工作日，结束为假日", {
-})
-
-test_that("计算工作日时，过程中包含要排除的时段", {
+test_that("过程中包含要排除的时段", {
 })
